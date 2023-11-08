7c15ab548ca81a2a599bd8d86a797ae539a9f661
Your Name <you@example.com>
2023-11-08 12:24:27 +0300

    ,,

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:emlak/functions/duygu_nav.dart';
 import 'package:emlak/screens/base_scafold.dart';
+import 'package:emlak/screens/home_screen.dart';
+import 'package:emlak/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -13,7 +16,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'emlak',
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => const SplashScreen(),
+        '/home': (context) => const HomeScreen(),
+      },
       debugShowCheckedModeBanner: false,
+      navigatorKey: DuyguNav.navigatorKey,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,41 @@
+import 'package:emlak/functions/duygu_nav.dart';
+import 'package:emlak/screens/base_scafold.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  Future init() async => Future.delayed(
+        const Duration(seconds: 3),
+        () => DuyguNav.pushAndPopBack(const BaseScaffold()),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TweenAnimationBuilder<double>(
+          tween: Tween<double>(begin: 16.0, end: 36),
+          duration: const Duration(seconds: 2),
+          builder: (BuildContext context, double size, Widget? child) {
+            return Text(
+              'Hoşgeldiniz',
+              style: TextStyle(fontSize: size, color: Colors.blue),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
